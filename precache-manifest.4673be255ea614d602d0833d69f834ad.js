80fb2e787d23171e5af84d6be2db670cd2edf974
Paweł Szymczak <mangoman@wp.pl>
2020-07-10 21:52:40 +0200

    Updates

---
+++ precache-manifest.4673be255ea614d602d0833d69f834ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bead9f33a036b97375e5c8e2f7789e4",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "22e0801022c89376e4ce",
+    "url": "/todos-list-react/static/css/main.3d0f2e44.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e0801022c89376e4ce",
+    "url": "/todos-list-react/static/js/main.21ea4e1f.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
